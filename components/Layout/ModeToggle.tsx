815ed5dab48d908d3e478be5a42061e8a1710f5c
shafayatGit <shafayathossain.ielts@gmail.com>
2026-04-03 00:51:53 +0600

    modetoggle updated

--- components/Layout/ModeToggle.tsx
+++ components/Layout/ModeToggle.tsx
@@ -16,12 +16,11 @@ export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger render={<Button variant="outline" size="icon" />}>
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
-      </DropdownMenuTrigger>
+    <>
+     <div className="container">
+  <input type="checkbox" name="checkbox" id="checkbox" />
+  <label for="checkbox" class="label"> </label>
+</div>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
@@ -33,6 +32,6 @@ export function ModeToggle() {
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
-    </DropdownMenu>
+    </>
   );
 }
