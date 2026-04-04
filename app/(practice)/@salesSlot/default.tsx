53d5aa1a8087b75ff62dd331e9d43e179252d637
shafayatGit <shafayathossain.ielts@gmail.com>
2026-04-04 14:45:09 +0600

    partial rendaring

---
+++ app/(practice)/@salesSlot/default.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SalesDefault() {
+  return (
+    <div>SalesDefault</div>
+  )
+}
--- app/(practice)/layout.tsx
+++ app/(practice)/layout.tsx
@@ -3,16 +3,24 @@ import React from "react";
 
 export default function PracticeLayout({
   children,
+  marketingSlot,
+  salesSlot,
 }: {
   children: React.ReactNode;
+  marketingSlot: React.ReactNode;
+  salesSlot: React.ReactNode;
 }) {
   return (
     <div>
       <div className="flex gap-10 m-8">
-        <Link href={"/development"}>Button</Link>
+        <Link href={"/development"}>Development</Link>
         <Link href={"/marketing"}>Marketing</Link>
         <Link href={"/marketing/settings"}>Settings</Link>
         <Link href={"/sales"}>Sales</Link>
+      </div>
+      <div className="flex">
+        {marketingSlot}
+        {salesSlot}
       </div>
       {children}
     </div>
