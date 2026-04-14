5729381d65bc05764203c8c15b9a04431d67dcdd
shafayatGit <shafayathossain.ielts@gmail.com>
2026-04-14 11:49:32 +0600

    getting user session

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,9 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session = await authClient.getSession();
-  console.log(session);
+  const cookieStore = await cookies();
+  const res = await fetch("http://localhost:8000/api/auth/get-session", {
+    headers: {
+      cookie: cookieStore.toString(), //map take string akare pathaye dey
+    },
+    cache: "no-store", // We dont want to cache the user data
+  });
+  const session = await res.json();
+  console.log(session.user);
+
   return (
     <div>
       <Button variant="destructive">Click Here</Button>
