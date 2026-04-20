06eedf31509a0a69dd696debf037074f38e2ca29
shafayatGit <shafayathossain.ielts@gmail.com>
2026-04-20 23:55:13 +0600

    fetch best approach

--- src/services/blog.services.ts
+++ src/services/blog.services.ts
@@ -5,7 +5,9 @@ const API_URL = env.APP_URL;
 export const blogService = {
   getBlogPosts: async function () {
     try {
-      const res = await fetch(`${API_URL}/posts`);
+      //   const res = await fetch(`${API_URL}/posts`,{cache:"no-store"});
+      const res = await fetch(`${API_URL}/posts`, { next: { revalidate: 60 } }); //ISR (Incremental Static Regeneration) 60 means after 60 seconds it will revalidate the data and update the cache
+
       const data = await res.json();
 
       return { data: data, error: null };
