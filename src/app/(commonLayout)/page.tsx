f918b10a5269ae50064a9c134748001c7c2e0d0c
shafayatGit <shafayathossain.ielts@gmail.com>
2026-04-21 00:23:31 +0600

    best approach for searching params

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -3,7 +3,15 @@ import { blogService } from "@/src/services/blog.services";
 import { BlogsType } from "@/src/types";
 
 export default async function Home() {
-  const { data } = await blogService.getBlogPosts();
+  const { data } = await blogService.getBlogPosts(
+    {
+      isFeatured: true,
+    },
+    {
+      // cache: "no-store",
+      revalidate: 10,
+    },
+  );
   // console.log(data.data.data)
   return (
     <div className="grid grid-cols-3 gap-4 ">
--- src/services/blog.services.ts
+++ src/services/blog.services.ts
@@ -6,12 +6,46 @@ const API_URL = env.APP_URL;
 //{ cache: "no-store" } -> SSR -> Server Side Rendering
 //{ next: { revalidate: 60 } } -> ISR -> Incremental Static Regeneration . Mix between static and dynamic. It will cache the data for 60 seconds and after that it will revalidate the data and update the cache. So we can get the benefits of both static and dynamic rendering.
 
+interface ServiceOptions {
+  cache?: RequestCache; // RequestCache is a built-in TypeScript type that represents the cache option for the fetch API. It can be "default", "no-store", "reload", "no-cache", "force-cache", or "only-if-cached".
+  revalidate?: number;
+}
+
+interface BlogBlogParams {
+  search?: string;
+  isFeatured?: boolean;
+}
+
 export const blogService = {
-  getBlogPosts: async function () {
+  getBlogPosts: async function (
+    params?: BlogBlogParams,
+    options?: ServiceOptions,
+  ) {
     try {
       //   const res = await fetch(`${API_URL}/posts`,{cache:"no-store"});
-      const res = await fetch(`${API_URL}/posts`, { next: { revalidate: 60 } }); //ISR (Incremental Static Regeneration) 60 means after 60 seconds it will revalidate the data and update the cache
+      //   const res = await fetch(url.toString(), { next: { revalidate: 60 } }); //ISR (Incremental Static Regeneration) 60 means after 60 seconds it will revalidate the data and update the cache
 
+      //------->>>>>>Best approach:
+
+      const url = new URL(`${API_URL}/posts`);
+      if (params) {
+        Object.entries(params).forEach(([key, value]) => {
+          if (value !== undefined && value !== null && value !== "") {
+            url.searchParams.append(key, value);
+          }
+        });
+      }
+
+      //
+      const config: RequestInit = {}; //We will pass this config to fetch function and we will add the cache and revalidate options to this config object based on the options passed to the function
+      if (options?.cache) {
+        config.cache = options.cache; //We can set the cache option to "no-store" to disable caching or "force-cache" to force caching or "default" to use the default caching behavior of the browser
+      }
+      if (options?.revalidate) {
+        config.next = { revalidate: options.revalidate }; //We can set the revalidate option to a number in seconds to enable ISR (Incremental Static Regeneration) or we can set it to "force-cache" to force caching or "default" to use the default caching behavior of the browser
+      }
+
+      const res = await fetch(url.toString(), config);
       const data = await res.json();
 
       return { data: data, error: null };
